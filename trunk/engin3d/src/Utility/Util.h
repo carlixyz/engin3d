c76548906a6e906a6c96c7bee3cf4312b07adbf5
manolopm <manolopm@8d52fbf8-9d52-835b-178e-190adb01ab0c>
2010-10-05 22:07:52 +0000

    Utility class to handle multiplatform time functions (and more things in the future)

---
+++ trunk/engin3d/src/Utility/Util.h
@@ -0,0 +1,16 @@
+#ifndef UTIL_H
+#define	UTIL_H
+#ifdef _WIN32
+  #include <windows.h>
+  #define GetMiliSeconds() timeGetTime()
+
+
+#else
+  #include <sys/time.h>
+  float GetMiliSeconds(){
+    timeval ltTime;
+    gettimeofday(&ltTime, NULL);
+    return ltTime.tv_usec/1000.0f;
+  }
+#endif
+#endif
