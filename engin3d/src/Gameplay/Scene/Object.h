02711a5c784ff22da19eb37cd21e997750b3d62b
manolopm <manolopm@8d52fbf8-9d52-835b-178e-190adb01ab0c>
2011-01-16 15:36:18 +0000

    Last part finished

---
+++ engin3d/src/Gameplay/Scene/Object.h
@@ -0,0 +1,26 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+#include "../../Utility/ResourceHandle.h"
+#include "../../MathLib/Matrix/Matrix.h"
+
+class cObject
+{
+public:
+  void Init();
+  void Deinit(){};
+  virtual void Update( float lfTimeStep){};
+  virtual void Render();
+  inline std::string GetName() { return macName;}
+  inline void SetName( const std::string &lacName) {macName = lacName; }
+  inline void SetWorldMatrix( const cMatrix& lWorld) { mWorldMatrix = lWorld; }
+  inline cMatrix GetWorldMatrix ( const cMatrix& lWorld) { return mWorldMatrix; }
+  void AddMesh ( cResourceHandle lMeshHandle, cResourceHandle lMaterialHandle );
+protected:
+  std::string macName;
+  cMatrix mWorldMatrix;
+
+  std::vector <cResourceHandle> mMeshHandles;
+  std::vector <cResourceHandle> mMaterialHandles;
+};
+
+#endif
