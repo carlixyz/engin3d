777457ee30b421022a650cf83f791dbbb9655947
carlixyz@yahoo.com <carlixyz@yahoo.com@8d52fbf8-9d52-835b-178e-190adb01ab0c>
2011-04-20 21:50:56 +0000

    Working version with nvidia cg.lib & .cgGL.lib includes added

--- trunk/engin3d/src/Graphics/GLHeaders.h
+++ trunk/engin3d/src/Graphics/GLHeaders.h
@@ -20,5 +20,5 @@
 #else
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
-/*
+*/
 // Another option is : go Project Properties > Herramienta Manifiesto > Utilizar archivos de respuesta UNICODE > NO !
